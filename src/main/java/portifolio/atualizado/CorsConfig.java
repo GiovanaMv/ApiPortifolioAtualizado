b5f89d9a1fd8be4fa4d7c3e7838680d7f07e6b7a
GiovanaMv <saddnez@gmail.com>
2026-01-22 14:59:03 -0300

    feat: add configuração global de CORS para permitir acesso do front-end hospedado na Vercel

--- src/main/java/portifolio/atualizado/CorsConfig.java
+++ src/main/java/portifolio/atualizado/CorsConfig.java
@@ -7,13 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://portfolio-atualizado-swart.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
